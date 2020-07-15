72df84a3e61d5e3dec07100b1214eda0915eb8cf
wangpeng <781757836@qq.com>
2020-07-15 16:15:51 +0800

    add models

--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+
+
+def add2(request, a, b):
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+
+
 def index(request):
-    return HttpResponse('Hello, world. You are at the polls index')
+    return HttpResponse('hello, Django')
