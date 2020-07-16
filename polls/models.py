a0065f1fb0d518cad28c6f71a82f2541ef2988d4
wangpeng <781757836@qq.com>
2020-07-16 17:57:15 +0800

    add static

--- polls/models.py
+++ polls/models.py
@@ -15,7 +15,11 @@ class Question(models.Model):
 
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        return now - datetime.timedelta(days=1) <= datetime.datetime(self.pub_date.year, self.pub_date.month,
+                                                                     self.pub_date.day) <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
