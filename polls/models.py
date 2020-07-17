404e24e7f797a263d706860c65931e27c3114c7a
wangpeng <781757836@qq.com>
2020-07-17 16:53:54 +0800

    change admin & add templates

--- polls/models.py
+++ polls/models.py
@@ -8,15 +8,14 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateField('date published')
+    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days=1) <= datetime.datetime(self.pub_date.year, self.pub_date.month,
-                                                                     self.pub_date.day) <= now
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
